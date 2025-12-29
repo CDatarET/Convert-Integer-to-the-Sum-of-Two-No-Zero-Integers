5e7ff4b3cc8de06115396877ef5dbf7b34988193
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-12-29 21:00:36 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    private boolean hasZero(int n){
+        while(n > 0){
+            if(n % 10 == 0) return true;
+            n = n / 10;
+        }
+        return false;
+    }
+
+    public int[] getNoZeroIntegers(int n) {
+        int r = n - 1;
+        for(int i = 1; i <= n / 2; i++){
+            if(i + r == n){
+                if(!hasZero(i) && !hasZero(r)){
+                    return(new int[] {i, r});
+                }
+            }
+
+            r--;
+        }
+        return(null);
+    }
+}
